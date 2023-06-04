a3eb19a116851622cc295fd0796de7c7281bbd63
chajus1 <chajus1@googlemail.com>
2023-06-04 06:22:34 +0100

    complete

--- QPoolPuzzle/Program.cs
+++ QPoolPuzzle/Program.cs
@@ -10,6 +10,22 @@ namespace QPoolPuzzle
     {
         static void Main(string[] args)
         {
+            Q q = new Q(Q.R.Next(2) == 1);
+            while (true)
+            {
+                Console.Write($"{q.N1} {q.Op} {q.N2} = ");
+                if(!int.TryParse(Console.ReadLine(), out int i))
+                {
+                    Console.WriteLine("Thanks for Playing!");
+                    return;
+                }
+                if (q.Check(i))
+                {
+                    Console.WriteLine("Right!");
+                    q = new Q(Q.R.Next(2) == 1);
+                }
+                else Console.WriteLine("Wrong! Try again.");
+            }
         }
     }
 }
